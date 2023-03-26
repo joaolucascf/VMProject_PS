2d7e0e4a00bfb71d51096c0b950404b1bd17bef2
Vaipo <joaolucasdacf@gmail.com>
2023-03-26 13:42:09 -0300

    nem sei mais oq to fazendo

---
+++ src/main/java/com/vm/ps/vmproject_ps/DataMemView.java
@@ -0,0 +1,14 @@
+package com.vm.ps.vmproject_ps;
+
+import javafx.scene.chart.PieChart;
+
+public class DataMemView{
+    String memColumn0;
+    String memColumn1;
+    String memColumn2;
+    public DataMemView(String mem0, String mem1, String mem2){
+        memColumn0 = mem0;
+        memColumn1 = mem1;
+        memColumn2 = mem2;
+    }
+}
--- src/main/java/com/vm/ps/vmproject_ps/ULA.java
+++ src/main/java/com/vm/ps/vmproject_ps/ULA.java
@@ -1,5 +1,4 @@
 package com.vm.ps.vmproject_ps;
-
 import java.util.*;
 
 public class ULA {
@@ -10,7 +9,6 @@ STA ALPHA
 LDCH CHARZ
 STCH C1
 STOP
-
 ALPHA RESW 1
 FIVE WORD 5
 CHARZ BYTE C'Z'
@@ -91,16 +89,24 @@ C1 RESB 1
                 System.out.println(reg.registerSet.get("A").getValue());
                 break;
             case 180:
+            	String[] regs2 = operand.trim().toUpperCase().split(",");
+            	reg.registerSet.get(regs2[0]).setValue(0);
                 break;
             case 40:
-                break;
+            	reg.registerSet.get("A");
+            	break;
             case 160:
                 break;
             case 36:
                 break;
             case 156:
+            	String[] regs3 = operand.trim().toUpperCase().split(",");
+            	reg.registerSet.get(regs3[0]).setValue(2);
+            	reg.registerSet.get(regs3[1]).setValue(3);
+            	reg.registerSet.get(regs3[0]).setValue(reg.registerSet.get(regs3[0]).getValue()/reg.registerSet.get(regs3[1]).getValue());
                 break;
             case 60:
+            	reg.registerSet.get("PC").setValue(Byte.parseByte(operand));
                 break;
             case 48:
                 break;
@@ -111,30 +117,48 @@ C1 RESB 1
             case 72:
                 break;
             case 0:
+            	reg.registerSet.get("A").setValue(Byte.parseByte(operand, 2));
                 break;
             case 104:
+            	reg.registerSet.get("B").setValue(Byte.parseByte(operand, 2));
                 break;
             case 80:
                 break;
             case 8:
+            	reg.registerSet.get("L").setValue(Byte.parseByte(operand, 2));
                 break;
             case 108:
+            	reg.registerSet.get("S").setValue(Byte.parseByte(operand, 2));
                 break;
             case 116:
+            	reg.registerSet.get("T").setValue(Byte.parseByte(operand, 2));
                 break;
             case 4:
+            	reg.registerSet.get("X").setValue(Byte.parseByte(operand, 2));
                 break;
             case 32:
+            	 reg.registerSet.get("A").setValue(reg.registerSet.get("A").getValue()*Byte.parseByte(operand,2));	
                 break;
             case 152:
+            	String[] regs4 = operand.trim().toUpperCase().split(",");
+            	reg.registerSet.get(regs4[0]).setValue(2);
+            	reg.registerSet.get(regs4[1]).setValue(3);
+            	reg.registerSet.get(regs4[0]).setValue(reg.registerSet.get(regs4[0]).getValue()*reg.registerSet.get(regs4[1]).getValue());
                 break;
             case 68:
+            	reg.registerSet.get("A").setValue(reg.registerSet.get("A").getValue()| Byte.parseByte(operand,2));	
                 break;
             case 172:
+            	String [] regs5 = operand.trim().toUpperCase().split(",");
+            	reg.registerSet.get(regs5[0]).setValue(2);
+            	reg.registerSet.get(regs5[1]).setValue(3);
+            	reg.registerSet.get(regs5[0]).setValue(reg.registerSet.get(regs5[1]).getValue());
                 break;
             case 76:
+            	//reg.registerSet.get("PC").setValue();
                 break;
             case 164:
+           // 	reg.registerSet.get(Byte.parseByte(operand, 2)).setValue(A);
                 break;
             case 168:
                 break;
@@ -153,20 +177,30 @@ C1 RESB 1
             case 16:
                 break;
             case 28:
+            	reg.registerSet.get("A").setValue(reg.registerSet.get("A").getValue() - Byte.parseByte(operand,2));
                 break;
             case 148:
+            	String[] regs6 = operand.trim().toUpperCase().split(",");
+            	reg.registerSet.get(regs6[0]).setValue(2);
+            	reg.registerSet.get(regs6[1]).setValue(3);
+            	reg.registerSet.get(regs6[0]).setValue(reg.registerSet.get(regs6[0]).getValue()*reg.registerSet.get(regs6[1]).getValue());
                 break;
             case 44:
+            	
                 break;
             case 184:
                 break;
             default:
-                throw new RuntimeException("C√≥digo de opera√ß√£o inv√°lido");
+                throw new RuntimeException("CÛdigo de operaÁ„o inv·lido");
         }
     }
     public void printData(){
         for(String i : mem.dataMemory){
             System.out.println(i);
         }
+    }
+
+    public Memory getMem(){
+        return mem;
     }
 }
